a28608cb9b53a4131067e52be9ca765c07a5be99
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-28 14:37:44 -0500

    set data base

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Auth from './components/Auth';
+import { db } from "./config/firebase"
+import { getDocs, collection } from 'firebase/firestore';
+
 
 function App() {
+  const [movieList, setMovieList] = useState([])
+  
+  const moviesCollectionRef = collection(db, "movies")
+
+  useEffect(() => {
+    const getMovieList = async () => {
+      //Read the data from the db
+      // set movie list
+      try {
+        const data = await getDocs(moviesCollectionRef)
+        console.log(data)
+      } catch (err) {
+        console.error(err)
+      }
+      
+    }
+    getMovieList()
+  }, [])
+  
+
   return (
     <div className="App">
       <Auth />
